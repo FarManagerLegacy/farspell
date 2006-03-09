197cce501e904b1c2f76f8d192917a516bb2d31f
sergey shishmintzev <sergey.shishmintzev@gmail.com>
2006-03-09 09:25:49 -0200

    Proper handling of special chars in middle and adjacent of spelling word (was parse-spec-1, see history.txt:20060309)

--- FarSpell/ParserFactory.hpp
+++ FarSpell/ParserFactory.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 class ParserInstance
 {
   friend class ParserFactory;
@@ -12,6 +11,8 @@ class ParserInstance
     int get_tokenpos();
     ~ParserInstance();
   private:
+    int last_token_skip;
+    FarString middle_chars;
     ParserInstance(TextParser *hunspell_parser);
     TextParser *parser;
 };
@@ -20,6 +21,8 @@ ParserInstance::ParserInstance(TextParser *hunspell_parser)
 {
   far_assert(hunspell_parser);
   parser = hunspell_parser;
+  middle_chars = "-'";
+  last_token_skip = 0;
 }
 
 ParserInstance::~ParserInstance() 
@@ -39,13 +42,50 @@ void ParserInstance::put_line(char *line)
 char *ParserInstance::next_token()
 {
   far_assert(parser);
-  return parser->next_token();
+  char *token = NULL; 
+  char *skip_begin, *skip_end;
+  last_token_skip = 0;
+  for (;;) {
+    if (token) hunspell_free(token);
+    token = parser->next_token();     
+    if (!token) return NULL;  // все слова кончились
+    far_assert(*token);
+    // пропустим все спец-символы в начале слова:
+    for (skip_begin = token; 
+         *skip_begin && middle_chars.IndexOf(*skip_begin)>=0; 
+         skip_begin++);
+    if (!*skip_begin) continue; // группа спец-символов - это не слово...
+
+    // опустим все спец-символы в конце слова:
+    for (skip_end = token+strlen(token)-1; 
+         skip_end!=token && middle_chars.IndexOf(*skip_end)>=0; 
+         skip_end--);
+    *(skip_end+1) = '\0'; // обрежем спец-символы в конце слова.
+    far_assert(skip_begin <= skip_end);
+    // вначале слова ничего не пропустили, поэтому вернём адрес
+    // который может и должен быть освобождён hunspell_free.
+    if (skip_begin == token) 
+      return token;
+    // увы, придётся выделить память заново, чтобы вызывающая
+    // процедура могла освободить слово с помощью hunspell_free.
+    int len = skip_end-skip_begin+1;
+    far_assert(len!=0); // это надо отловить и исправить.
+    far_assert(len>0); 
+    char *new_token = (char*)hunspell_malloc((len+1)*sizeof(char));
+    memcpy(new_token, skip_begin, (len+1)*sizeof(char));
+    far_assert(*(new_token+len) == '\0');  //*(new_token+len) = '\0';
+    hunspell_free(token);
+    last_token_skip = skip_begin-token; // TextParser не знает, 
+                                        // что мы обрезали слово
+    return new_token;
+  }
 }
 
 int ParserInstance::get_tokenpos()
 {
   far_assert(parser);
-  return parser->get_tokenpos();
+  far_assert(last_token_skip>=0);
+  return parser->get_tokenpos() + last_token_skip;
 }
 
 class ParserFactory
